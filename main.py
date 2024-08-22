6eed6cea8b21e7df31ff84aa9cd50e22e5ef56a4
HyunOh Kim <gaussian88@naver.com>
2024-08-22 12:28:46 +0900

    첫번째

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from extractors.wanted import extract_wanted_jobs
-from file import save_to_file
+# from extractors.wanted import extract_wanted_jobs
+# from file import save_to_file
 
-keyword = input("무슨 직종을 검색 하시겠습니까?\n")
+# keyword = input("무슨 직종을 검색 하시겠습니까?\n")
 
-jobs = extract_wanted_jobs(keyword)
+# jobs = extract_wanted_jobs(keyword)
 
-save_to_file(keyword, jobs)
+# save_to_file(keyword, jobs)
+
+from flask import Flask, render_template 
+
+app = Flask("JobScrapper")
+
+@app.route("/")
+def home():
+    return render_template("home.html", name = "최은석")
+    
+
+@app.route("/search")
+def a_dir():
+    return render_template("search.html")
+
+app.run("0.0.0.0")
