0c06a3cf4b70e44f7c8423d46f984e040355fb13
surluka <surluka@naver.com>
2024-08-22 17:26:06 +0900

    .

--- main.py
+++ main.py
@@ -7,10 +7,13 @@
 
 # save_to_file(keyword, jobs)
 
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request, redirect , send_file
 from extractors.wanted import extract_wanted_jobs
+from file import save_to_file
 
 app = Flask("JobScrapper")
+
+db = {}  # 딕셔너리의 구조를 모르면 헤깔릴 수 있음
 
 @app.route("/")
 def home():
@@ -18,9 +21,34 @@ def home():
 
 
 @app.route("/search")
-def a_dir():
+def search():
+    
     keyword = request.args.get("keyword")
-    jobs = extract_wanted_jobs(keyword)
+    
+    if keyword == None:
+        return redirect("/")
+    
+    if keyword in db:                          # db 의 키값에서 키워드가 있다면
+        jobs = db[keyword]                     # 그 키의 밸류를 jobs 로 한다 가 된다 밸류값이 리스트로 엄청 길겠지?
+    else:
+        jobs = extract_wanted_jobs(keyword)
+        db[keyword] = jobs
+        print(db) # 요렇게 해 보면 키워드키값에 대한 밸류값을 볼 수 있다....밸류는 당연하게 리스트안에 딕셔너리가 들어가 있는 형태가 됨
     return render_template("search.html", keyword = keyword, jobs = jobs)
+
+@app.route("/export")
+def export():
+
+    keyword = request.args.get("keyword")
+
+    if keyword == None:
+        return redirect("/")
+
+    if keyword not in db:
+        return redirect(f"/search?keyword={keyword}")
+
+    save_to_file(keyword, db[keyword])
+
+    return send_file(f"{keyword}.csv", as_attachment=True)
 
 app.run("0.0.0.0")
